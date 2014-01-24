4d14e11ee0f6932606194a43548106ed6496a20b
Marc-André Moreau <marcandre.moreau@gmail.com>
2014-01-23 23:03:33 -0500

    VmBusPipe: more experiments

--- VmBusPipe/Main.c
+++ VmBusPipe/Main.c
@@ -7,6 +7,9 @@
 
 #include "VmBusPipe.h"
 
+// {f9e9c0d3-b511-4a48-8046-d38079a8830c} "Microsoft Hyper-V Remote Desktop Data Channel"
+DEFINE_GUID(GUID_VMBUS_RDP_DATA_CHANNEL, 0xf9e9c0d3, 0xb511, 0x4a48, 0x80, 0x46, 0xd3, 0x80, 0x79, 0xa8, 0x83, 0x0c);
+
 // {1DAE6E2C-D5EF-4085-8C3C-00FC462F181A}
 DEFINE_GUID(GUID_VMBUS_PIPE_TEST, 0x1dae6e2c, 0xd5ef, 0x4085, 0x8c, 0x3c, 0x0, 0xfc, 0x46, 0x2f, 0x18, 0x1a);
 
@@ -16,24 +19,73 @@ static BYTE RdvVmEndPointInstanceGuidUnifiedApi[16] =
 static BYTE RdvVmEndPointInstanceGuidAllowList[16] =
 	"\x67\x97\x16\x6E\xF8\x53\xB7\x49\xBF\xE1\xB4\x99\x16\x80\x57\xC4";
 
+void HexDump(BYTE* data, int length)
+{
+	BYTE* p = data;
+	int i, line, offset = 0;
+
+	while (offset < length)
+	{
+		fprintf(stderr, "%04x ", offset);
+
+		line = length - offset;
+
+		if (line > 16)
+			line = 16;
+
+		for (i = 0; i < line; i++)
+			fprintf(stderr, "%02x ", p[i]);
+
+		for (; i < 16; i++)
+			fprintf(stderr, "   ");
+
+		for (i = 0; i < line; i++)
+			fprintf(stderr, "%c", (p[i] >= 0x20 && p[i] < 0x7F) ? p[i] : '.');
+
+		fprintf(stderr, "\n");
+
+		offset += line;
+		p += line;
+	}
+}
+
+void VmbusPipeClientEnumeratePipe(DWORD arg1, BYTE* arg2, int* arg3, char* arg4)
+{
+	printf("VmbusPipeClientEnumeratePipe: arg1: 0x%04X arg2: %p arg3: %p arg4: %p\n", arg1, arg2, arg3, arg4);
+}
+
 int test_VmBusPipeHost(VmBusPipeHost* host)
 {
 	int status;
 	BOOL bSuccess;
-	printf("VmBusPipeHost\n");
+	DWORD dwFlagsAndAttributes;
+	VMBUS_PIPE_CHANNEL_INFO* pChannelInfo;
+
+	printf("VmBusPipeHost: %d\n", host ? 1 : 0);
 
 	/**
 	 * VmbusPipeServerOfferChannel
 	 * VmbusPipeServerConnectPipe
 	 */ 
 
-	bSuccess = host->VmbusPipeClientEnumeratePipes((const GUID*) RdvVmEndPointInstanceGuidUnifiedApi, 0xFFFF, 0xFFFF);
+	pChannelInfo = (VMBUS_PIPE_CHANNEL_INFO*) malloc(sizeof(VMBUS_PIPE_CHANNEL_INFO));
+	ZeroMemory(pChannelInfo, sizeof(VMBUS_PIPE_CHANNEL_INFO));
+
+	bSuccess = host->VmbusPipeClientEnumeratePipes(&GUID_VMBUS_RDP_DATA_CHANNEL, 0, VmbusPipeClientEnumeratePipe);
 
 	printf("VmbusPipeClientEnumeratePipes: %d\n", bSuccess);
 
-	status = host->VmbusPipeServerOfferChannel(NULL, 0xFFFF, 0xFFFF);
+	pChannelInfo->flags3A = 0x1000;
+	dwFlagsAndAttributes = 0;
+
+	status = host->VmbusPipeServerOfferChannel(pChannelInfo, dwFlagsAndAttributes, 0);
 
 	printf("VmbusPipeServerOfferChannel: %d\n", status);
+
+	if (status < 0)
+	{
+		printf("VmbusPipeServerOfferChannel: GetLastError() = %d\n", GetLastError());
+	}
 
 	return 0;
 }
@@ -43,8 +95,10 @@ int test_VmBusPipeGuest(VmBusPipeGuest* guest)
 	int status;
 	BOOL bSuccess;
 	HANDLE hChannel;
+	DWORD dwFlagsAndAttributes;
+	VMBUS_PIPE_CHANNEL_INFO* pChannelInfo;
 
-	printf("VmBusPipeGuest\n");
+	printf("VmBusPipeGuest: %d\n", guest ? 1 : 0);
 
 	/**
 	 * VmbusPipeClientOpenChannel
@@ -53,39 +107,75 @@ int test_VmBusPipeGuest(VmBusPipeGuest* guest)
 	 * VmbusPipeClientEnumeratePipes
 	 */ 
 
-	bSuccess = guest->VmbusPipeClientEnumeratePipes((const GUID*) RdvVmEndPointInstanceGuidUnifiedApi, 0xFFFF, 0xFFFF);
+	pChannelInfo = (VMBUS_PIPE_CHANNEL_INFO*) malloc(sizeof(VMBUS_PIPE_CHANNEL_INFO));
+	ZeroMemory(pChannelInfo, sizeof(VMBUS_PIPE_CHANNEL_INFO));
+
+	bSuccess = guest->VmbusPipeClientEnumeratePipes(&GUID_VMBUS_RDP_DATA_CHANNEL, 0, VmbusPipeClientEnumeratePipe);
 
 	printf("VmbusPipeClientEnumeratePipes: %d\n", bSuccess);
 
-	status = guest->VmbusPipeServerOfferChannel(NULL, 0xFFFF, 0xFFFF);
+	pChannelInfo->flags3A = 0x1000;
+	dwFlagsAndAttributes = 0;
+
+	/**
+	 * CreateFile dwFlagsAndAttributes:
+	 * http://msdn.microsoft.com/en-us/library/windows/desktop/aa363858/
+	 * FILE_FLAG_POSIX_SEMANTICS (0x0100000) is added
+	 */
+
+	status = guest->VmbusPipeServerOfferChannel(pChannelInfo, dwFlagsAndAttributes, 0);
 
 	printf("VmbusPipeServerOfferChannel: %d\n", status);
+
+	if (status < 0)
+	{
+		/**
+		 * Guest: ERROR_FILE_NOT_FOUND
+		 *
+		 * Host: ERROR_ACCESS_DENIED
+		 * Host + Admin: ERROR_INVALID_FUNCTION
+		 */
+
+		printf("VmbusPipeServerOfferChannel: GetLastError() = %d\n", GetLastError());
+	}
 
 	/**
 	 * First argument is a 526-byte structure
 	 * Second argument is a set of flags, with an observed value of 0x40000000
 	 */
 
-	hChannel = guest->VmbusPipeClientOpenChannel(NULL, 0x40000000);
+	hChannel = guest->VmbusPipeClientOpenChannel(pChannelInfo, 0x40000000);
 
 	printf("VmbusPipeClientOpenChannel: 0x%04X\n", hChannel);
+
+	free(pChannelInfo);
 
 	return 0;
 }
 
 int main(int argc, char** argv)
 {
+	BOOL bGuestMode;
 	VmBusPipeHost* host;
 	VmBusPipeGuest* guest;
 
+	bGuestMode = FALSE;
 	host = VmBusPipeHostInit();
 	guest = VmBusPipeGuestInit();
 
-	if (host)
+	if (argc > 1)
+	{
+		if (strcmp(argv[1], "guest") == 0)
+		{
+			bGuestMode = TRUE;
+		}
+	}
+
+	if (host && !bGuestMode)
 	{
 		test_VmBusPipeHost(host);
 	}
-	else if (guest)
+	else if (guest && bGuestMode)
 	{
 		test_VmBusPipeGuest(guest);
 	}
